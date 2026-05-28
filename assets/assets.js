f88a443a356052ee9b595cc8c103cc637debf134
PRATIK TRIPATHI <144891520+PRATIK5311@users.noreply.github.com>
2026-05-28 16:30:29 +0530

    first11

--- assets/assets.js
+++ assets/assets.js
@@ -22,7 +22,7 @@ import mail_icon_dark from "./mail_icon_dark.png";
 import profile from "./profile.png";
 import download_icon from "./download-icon.png";
 import hand_icon from "./hand-icon.png";
-import header_bg_color from "./header-bg-color.png";
+import header_bg_color from "./header-bg-color.png"; 
 import moon_icon from "./moon_icon.png";
 import sun_icon from "./sun_icon.png";
 import arrow_icon from "./arrow-icon.png";
